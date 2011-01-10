f68429d86dcae6d00537bc48cea02371cd8ffcb9
gardnerm <gardnerm@01a83866-e137-448d-94ba-132f31b327cc>
2011-01-10 21:06:32 +0000

    preliminary code re-implementing component/branch creatuon in terms of DBIx::Class and Try::Tiny
    git-svn-id: http://devsvn.gspt.net/svn/operations/shared_services/gsi-content-cmd/trunk@4068 01a83866-e137-448d-94ba-132f31b327cc

--- lib/GSI/Content/Cmd/Command/check_lock.pm
+++ lib/GSI/Content/Cmd/Command/check_lock.pm
@@ -3,16 +3,21 @@ package GSI::Content::Cmd::Command::check_lock;
 
 # ABSTRACT: Subversion pre-commit hook for lock-to-author content
 
+use Carp;
 use English '-no_match_vars';
 use Moose;
 use MooseX::Has::Sugar;
 use Readonly;
+use Try::Tiny;
 use namespace::autoclean;
 
 extends 'MooseX::App::Cmd::Command';
 with 'GSI::Automerge::SchemaConnection';
 with 'GSI::Content::Config';
 with 'SVN::Simple::Hook::PreCommit' => { -version => 0.110100 };
+
+Readonly my $SVN_BRANCH => 'SVN_LTA_SUPPORT';
+Readonly my $DEFAULT_LTA_ID => 3;    # default lock status ID for new branches
 
 =method execute
 
@@ -22,6 +27,34 @@ Runs the subcommand.
 
 sub execute {
     my ( $self, $opt, $args ) = @ARG;
+
+    my $schema = $self->automerge();
+    my $repos  = $self->repository();
+
+    try {
+        my @COMPONENT_REPO = (
+            { component_name => $repos->path() },
+            { key            => 'scm_component_uk1' },
+        );
+        my $guard = $schema->txn_scope_guard();
+
+        my $component_rs = $schema->resultset('ScmComponent');
+        my $component    = $component_rs->find(@COMPONENT_REPO);
+        if ( !defined $component ) {
+            $component = $component_rs->find_or_create(@COMPONENT_REPO);
+            $schema->resultset('Branch')->create(
+                {   branch_name            => $SVN_BRANCH,
+                    component_id           => $component->component_id(),
+                    default_lock_status_id => $DEFAULT_LTA_ID,
+                }
+            ) or croak 'no branch record for ', $repos->path();
+        }
+
+        $guard->commit();
+    }
+    catch {
+        croak "database transaction aborted: $EVAL_ERROR";
+    };
 
     return;
 }
